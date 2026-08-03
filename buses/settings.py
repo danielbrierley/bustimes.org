d3c6a8e23cec99b6cc3dfba049b4fdb9714f6397
Josh Goodwin <j@joshuagoodw.in>
2026-08-03 11:59:52 +0100

    reverse c174e8b62bd553383b2f4e72dafbd9bb7229ba5d, reuse db connections while we're using wsgi again

--- buses/settings.py
+++ buses/settings.py
@@ -114,8 +114,9 @@ ROOT_URLCONF = "buses.urls"
 ASGI_APPLICATION = "buses.asgi.application"
 
 
-DATABASES = {"default": dj_database_url.config()}
-
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=None, conn_health_checks=True)
+}
 DATABASES["default"]["OPTIONS"] = {
     "application_name": os.environ.get("APPLICATION_NAME") or " ".join(sys.argv)[-63:],
     "connect_timeout": 9,
